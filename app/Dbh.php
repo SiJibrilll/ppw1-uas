5484e738df307ff808613e46c289c975e95b20bd
SiJibrilll <jibrilianwa@gmail.com>
2025-05-21 12:18:39 +0700

    Initial set up with Dbh, router, and index page

---
+++ app/Dbh.php
@@ -0,0 +1,34 @@
+<?php
+
+class Dbh {
+  private $conn;
+
+  function __construct() {
+    $servername = "127.0.0.1";
+    $dbName = 'restoranPOS';
+    $username = "root";
+    $password = "";
+
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+      $this->conn = $conn;
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+  }
+
+  public function query(string $sql, array $params = []) {
+        try {
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute($params);
+            return $stmt;
+        } catch (PDOException $e) {
+            // Log or handle the error appropriately
+            echo "PDO Error: " . $e->getMessage();
+            return false;
+        }
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+$root = __DIR__ . "/../";
+require_once $root . "app/Dbh.php";
+require_once $root . "app/router.php";
